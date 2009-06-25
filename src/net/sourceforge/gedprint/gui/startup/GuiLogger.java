5dca329cb9954ad0c64ab27da79e48c194b25946
Jens Hofschröer <1849220+nigjo@users.noreply.github.com>
2009-06-25 12:57:05 +0000

    Klassen zum Starten der Oberflaeche in eigenes Paket verschoben

---
+++ src/net/sourceforge/gedprint/gui/startup/GuiLogger.java
@@ -0,0 +1,134 @@
+package net.sourceforge.gedprint.gui.startup;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import net.sourceforge.gedprint.gui.GedPrintGui;
+
+/**
+ * Neue Klasse erstellt von hof. Erstellt am Jun 25, 2009, 2:04:12 PM
+ *
+ * @todo Hier fehlt die Beschreibung der Klasse.
+ *
+ * @author hof
+ */
+public class GuiLogger
+{
+  public static void initLogger()
+  {
+    String packName = GedPrintGui.class.getPackage().getName();
+    String baselogger = packName.substring(0, packName.lastIndexOf('.'));
+    Logger logger = Logger.getLogger(baselogger);
+    logger.setLevel(Level.ALL);
+    Formatter formatter = new Formatter()
+    {
+      String NL = System.getProperty("line.separator");
+
+      @Override
+      public String format(LogRecord record)
+      {
+        StringBuilder builder = new StringBuilder();
+        String clname = record.getSourceClassName();
+        builder.append(clname.substring(clname.lastIndexOf('.') + 1));
+        builder.append(": ");
+        String prefix = builder.toString();
+        // bisheriges loeschen
+        builder.setLength(0);
+
+        String message = record.getMessage();
+        if(record.getResourceBundle() != null)
+        {
+          message = record.getResourceBundle().getString(message);
+        }
+        else if(record.getResourceBundleName() != null)
+        {
+          message = ResourceBundle.getBundle(record.getResourceBundleName()).
+              getString(message);
+        }
+        Object[] params = record.getParameters();
+        if(params != null)
+          message = MessageFormat.format(message, params);
+        builder.append(message);
+
+        String logtext = builder.toString();
+        String[] lines = logtext.split(NL);
+        builder = new StringBuilder();
+        for(String line : lines)
+        {
+          builder.append(prefix);
+          builder.append(line);
+          builder.append(NL);
+        }
+        return builder.toString();
+      }
+
+    };
+    ConsoleHandler chandler = new ConsoleHandler();
+    chandler.setLevel(Level.ALL);
+    chandler.setFormatter(formatter);
+    logger.addHandler(chandler);
+
+    // Protokollausgabe auch in Datei
+    try
+    {
+      File logfile = new File("GedPrint.log");
+
+      ensureMaxSize(logfile, 1024 * 1024l);
+
+      FileHandler handler = new FileHandler(logfile.getName(), true);
+      handler.setFormatter(formatter);
+      logger.addHandler(handler);
+    }
+    catch(Exception ex)
+    {
+      Logger.getLogger(GedPrintGui.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  private static void ensureMaxSize(File logfile, long maxlength)
+      throws IOException
+  {
+    if(!logfile.exists() || logfile.length() < maxlength)
+      return;
+
+    File tempFile = File.createTempFile("~prune", ".log");
+    BufferedReader in = new BufferedReader(new FileReader(logfile));
+    try
+    {
+      BufferedWriter out = new BufferedWriter(new FileWriter(tempFile));
+      try
+      {
+        in.skip(logfile.length() - maxlength);
+        String zeile;
+        while(null != (zeile = in.readLine()))
+        {
+          out.write(zeile);
+          out.newLine();
+        }
+      }
+      finally
+      {
+        out.close();
+      }
+    }
+    finally
+    {
+      in.close();
+    }
+    if(!logfile.delete())
+      return;
+    tempFile.renameTo(logfile);
+  }
+
+}
---
+++ src/net/sourceforge/gedprint/gui/startup/GuiStartup.java
@@ -0,0 +1,159 @@
+package net.sourceforge.gedprint.gui.startup;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import net.sourceforge.gedprint.core.Messages;
+import net.sourceforge.gedprint.gui.GedFrame;
+import net.sourceforge.gedprint.gui.GedPrintGui;
+
+/**
+ * Neue Klasse erstellt von hof. Erstellt am Jun 25, 2009, 1:51:32 PM
+ *
+ * @todo Hier fehlt die Beschreibung der Klasse.
+ *
+ * @author hof
+ */
+public class GuiStartup implements Runnable
+{
+  private static final int CMD_OK = 0;
+  private static final int CMD_FILE = 1;
+  private static final int CMD_INDI = 2;
+  private static final int CMD_FAM = 3;
+  //File infile;
+  private static final String ARG_FILENAME = "filename";
+  private static final String ARG_INDIVIDUAL = "individual";
+  private static final String ARG_FAMILY = "family";
+  Properties arguments;
+
+  public GuiStartup()
+  {
+    try
+    {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }
+    catch(Exception ex)
+    {
+      // dann halt nicht.
+    }
+    Properties defaults = new Properties();
+    //defaults.setProperty(ARG_FILENAME, "");
+    //defaults.setProperty(ARG_INDIVIDUAL, "");
+    //defaults.setProperty(ARG_FAMILY, "");
+
+    arguments = new Properties(defaults);
+  }
+
+
+  public void run()
+  {
+    // DEBUG start
+    StringWriter sw = new StringWriter();
+    PrintWriter out = new PrintWriter(sw);
+    arguments.list(out);
+    Logger.getLogger(getClass().getName()).finer(sw.toString());
+    // DEBUG end
+
+    GedFrame frame = new GedFrame();
+
+    frame.setVisible(true);
+  }
+
+  public static void exit()
+  {
+    exit(0);
+  }
+
+  public static void exit(int rc)
+  {
+    System.exit(rc);
+  }
+
+  private void illegalArg(String pattern, String arg)
+  {
+    String title = Messages.getString("frame.title");
+    String message = MessageFormat.format(pattern, new Object[]
+        {
+          arg
+        });
+    JOptionPane.showMessageDialog(null, message, title,
+        JOptionPane.ERROR_MESSAGE);
+  }
+
+  public boolean parseCommandline(String[] args)
+  {
+    int status = CMD_OK;
+    for(String arg : args)
+    {
+      if(arg == null || arg.length() == 0)
+        continue;
+      if(arg.charAt(0) == '-')
+      {
+        if(status != CMD_OK)
+        {
+          illegalArg(Messages.getString("err.missingargument"), arg);
+          return false;
+        }
+        else if(arg.length() == 1)
+        {
+          illegalArg(Messages.getString("err.invalidarg"), arg);
+          return false;
+        }
+
+        switch(arg.charAt(1))
+        {
+          case 'i':
+            status = CMD_INDI;
+            break;
+          default:
+            illegalArg(Messages.getString("err.unknownoption"), arg);
+            return false;
+        }
+      }
+      else
+      {
+        switch(status)
+        {
+          case CMD_INDI:
+            arguments.setProperty(ARG_INDIVIDUAL, arg);
+            break;
+          case CMD_FAM:
+            arguments.setProperty(ARG_FAMILY, arg);
+            break;
+          case CMD_FILE:
+          default:
+            String infile = arguments.getProperty(ARG_FILENAME);
+            if(infile != null && infile.length() > 0)
+            {
+              illegalArg(Messages.getString("err.unknownargument"), arg);
+              return false;
+            }
+            arguments.setProperty(ARG_FILENAME, arg);
+            break;
+        }
+        status = CMD_OK;
+      }
+    }
+
+    return true;
+  }
+
+}
