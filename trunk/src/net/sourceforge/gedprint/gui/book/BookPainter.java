fe3ea57c9f0c6602a7680f0fb833580f8dae7cf9
nigjo <nigjo@1a3690dd-7231-0410-9601-9a37d12eefe4>
2009-07-04 10:29:48 +0000

    Neue Anzeigeklassen, die eine Seite aus dem "Familienbuch" verwenden.

---
+++ trunk/src/net/sourceforge/gedprint/gui/book/BookPainter.java
@@ -0,0 +1,52 @@
+package net.sourceforge.gedprint.gui.book;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+
+import net.sourceforge.gedprint.gedcom.Family;
+import net.sourceforge.gedprint.gedcom.Individual;
+import net.sourceforge.gedprint.gui.GedPainter;
+
+public class BookPainter extends GedPainter
+{
+  private static final long serialVersionUID = -5599769299072911707L;
+
+  JTabbedPane tabs;
+
+  public BookPainter()
+  {
+    super();
+    setLayout(new GridLayout(1, 1));
+    tabs = new JTabbedPane();
+    add(tabs);
+  }
+  
+  @Override
+  public boolean isScrollable()
+  {
+    return false;
+  }
+
+  @Override
+  public void add(Individual indi)
+  {
+    tabs.addTab(indi.toString(), new JPanel());
+  }
+
+  @Override
+  public void add(Family fam)
+  {
+    String title = fam.getIDCleared();
+    title += " - "; //$NON-NLS-1$
+    title += fam.getHusband().getClearedFullName();
+    title += "+"; //$NON-NLS-1$
+    title += fam.getWife().getClearedFullName();
+    
+    FamTab tab = new FamTab(fam);
+    tabs.addTab(title, new JScrollPane(tab));
+  }
+
+}
