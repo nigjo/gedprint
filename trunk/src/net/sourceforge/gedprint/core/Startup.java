99a5ee3d602687512b2285df86e5c7e20057de5a
nigjo <nigjo@1a3690dd-7231-0410-9601-9a37d12eefe4>
2009-08-09 20:36:10 +0000

    ExceptionEcho ist eigentständige Klasse

--- trunk/src/net/sourceforge/gedprint/core/Startup.java
+++ trunk/src/net/sourceforge/gedprint/core/Startup.java
@@ -1,12 +1,8 @@
 package net.sourceforge.gedprint.core;
 
-import java.text.MessageFormat;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.JOptionPane;
 
 public class Startup
 {
@@ -74,46 +70,6 @@ public class Startup
 
       Logger.getLogger(getClass().getName()).log(Level.SEVERE,
           e.getLocalizedMessage(), e);
-    }
-  }
-
-  private static class ExceptionEcho
-  {
-    private ResourceBundle bundle = ResourceBundle.getBundle(
-        "net.sourceforge.gedprint.core.ExceptionEcho"); //$NON-NLS-1$
-    private String message;
-
-    public ExceptionEcho(Throwable t)
-    {
-      String name = t.getClass().getSimpleName();
-      String pattern;
-      try
-      {
-        pattern = bundle.getString(name);
-      }
-      catch(MissingResourceException e)
-      {
-        pattern = bundle.getString("default"); //$NON-NLS-1$
-      }
-      Object[] args = new Object[2];
-      args[0] = t.getLocalizedMessage();
-      args[1] = t.toString();
-
-      message = MessageFormat.format(pattern, args);
-    }
-
-    @Override
-    public String toString()
-    {
-      return message;
-    }
-
-    public static void show(Throwable t)
-    {
-      ExceptionEcho echo = new ExceptionEcho(t);
-      t.printStackTrace();
-      JOptionPane.showMessageDialog(null, echo, t.getClass().getSimpleName(),
-          JOptionPane.ERROR_MESSAGE);
     }
   }
 }
