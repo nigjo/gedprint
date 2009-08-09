d59bf4ca82ccf0d54a4b01c4fee90eefc22b35dd
nigjo <nigjo@1a3690dd-7231-0410-9601-9a37d12eefe4>
2009-08-09 17:00:30 +0000

    simpler Wrapper für die Service-API

---
+++ trunk/src/net/sourceforge/gedprint/gui/core/Lookup.java
@@ -0,0 +1,30 @@
+package net.sourceforge.gedprint.gui.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * Utility Klasse fuer den ServiceLoader.
+ * 
+ * Aus "Java ist auch eine Insel", 8.Auflage, Kapitel 7.3.2
+ * 
+ * @author C. Ullenboom
+ */
+public class Lookup
+{
+  public static <T> T lookup(Class<T> clazz)
+  {
+    Iterator<T> iterator = ServiceLoader.load(clazz).iterator();
+    return iterator.hasNext() ? iterator.next() : null;
+  }
+
+  public static <T> Collection<? extends T> lookupAll(Class<T> clazz)
+  {
+    Collection<T> result = new ArrayList<T>();
+    for(T e : ServiceLoader.load(clazz))
+      result.add(e);
+    return result;
+  }
+}
