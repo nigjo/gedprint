ac70130bc5a5703d30c2fff02642f1c486fba3f7
nigjo <nigjo@1a3690dd-7231-0410-9601-9a37d12eefe4>
2009-08-12 12:14:51 +0000

    JavaDocs ergänzt

--- trunk/src/core/net/sourceforge/gedprint/core/Startup.java
+++ trunk/src/core/net/sourceforge/gedprint/core/Startup.java
@@ -3,82 +3,113 @@ package net.sourceforge.gedprint.core;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Startklasse der Anwendung.
+ * 
+ * @author Hofschr&ouml;er, Jens
+ */
 public class Startup
 {
   private static final String DEFAULT_MAIN =
       "net.sourceforge.gedprint.gui.core.GuiStartup"; //$NON-NLS-1$
 
   /**
-   * @param args
+   * Startet die Initialisierung.
    */
   public static void main(String[] args)
   {
-    String starterClassName = DEFAULT_MAIN;
-
-    // eventuellen Starter suchen
-    for(int i = 0; i < args.length; i++)
-    {
-      if(args[i].equals("--starter")) //$NON-NLS-1$
-      {
-        if(args.length >= i + 1)
-        {
-          starterClassName = args[i + 1];
-          String[] args2 = new String[args.length - 2];
-          if(i > 0)
-            System.arraycopy(args, 0, args2, 0, i);
-          if(i + 2 < args.length)
-            System.arraycopy(args, i + 2, args2, i, args2.length - i);
-
-          args = args2;
-          break;
-        }
-      }
-    }
-
-    // Klassenlader fuer externe Services
-    ClassLoader serviceLoader = new ServiceClassLoader(Thread.currentThread().
-        getContextClassLoader());
-
-    // Neuen Thread mit diesem Klassenlader initialisieren
-    GedPrintThreadGroup group = new GedPrintThreadGroup();
-    Runnable startup = new StartupRunner(starterClassName, args);
-    Thread gedthread = new Thread(group, startup, "startup");
-    gedthread.setContextClassLoader(serviceLoader);
-
-    // ... und starten
-    gedthread.start();
+    StartupRunner.startup(args);
   }
 
+  /**
+   * Initialisierungsklasse der Anwendung. Die Hilfsklasse preueft die
+   * Uebergebenen Parameter der Kommandozeile und startet die Anwendung
+   * in einem eigenen Thread. Dieser Thread verwendet einen
+   * {@link ServiceClassLoader} als Kontextlader um eventuelle externe
+   * .jar Dateien mit Serviceklassen dynamisch zu laden.
+   */
   private static class StartupRunner implements Runnable
   {
-    private String realStarterClassName;
-    private String[] realArgs;
 
-    public StartupRunner(String realStarterClassName, String[] realArgs)
+    private String starterClassName;
+    private String[] args;
+
+    public StartupRunner(String starterClassName, String[] args)
     {
-      this.realStarterClassName = realStarterClassName;
-      this.realArgs = realArgs;
+      this.starterClassName = starterClassName;
+      this.args = args;
     }
 
     public void run()
     {
       try
       {
-        Class starterClass = Class.forName(realStarterClassName);
+        Class starterClass = Class.forName(starterClassName);
         Object starterObject = starterClass.newInstance();
         GedPrintStarter starter = (GedPrintStarter)starterObject;
 
-        if(starter.parseCommandline(realArgs))
+        if(starter.parseCommandline(args))
           starter.run();
       }
       catch(Exception e)
       {
+        // Jede Exception waehrend der Initialisierung abfangen.
         ExceptionEcho.show(e);
       }
     }
 
+    /**
+     * Initialisierung der Anwendung. Test auf Startklasse und Generiung der
+     * eigenen ThreadGroup und Klassenlader.
+     *
+     * @param Vollstaendige Kommandozeile. Die Kommandozeile wird nach einem
+     *        Parameter "<code>--starter</code>" durchsucht und der nachfolgende
+     *        Parameter als Startklasse interpretiert. Diese beiden Argumente
+     *        werden im Folgenden nicht mehr an die Startklasse weitergereicht.
+     */
+    private static void startup(String[] args)
+    {
+      String starterClassName = DEFAULT_MAIN;
+
+      // eventuellen Starter suchen
+      for(int i = 0; i < args.length; i++)
+      {
+        if(args[i].equals("--starter")) //$NON-NLS-1$
+        {
+          if(args.length >= i + 1)
+          {
+            starterClassName = args[i + 1];
+            String[] args2 = new String[args.length - 2];
+            if(i > 0)
+              System.arraycopy(args, 0, args2, 0, i);
+            if(i + 2 < args.length)
+              System.arraycopy(args, i + 2, args2, i, args2.length - i);
+
+            // gefilterte Kommandozeile weiterreichen
+            args = args2;
+            break;
+          }
+        }
+      }
+
+      // Klassenlader fuer externe Services
+      ClassLoader serviceLoader = new ServiceClassLoader(Thread.currentThread().
+          getContextClassLoader());
+
+      // Neuen Thread mit diesem Klassenlader initialisieren
+      GedPrintThreadGroup group = new GedPrintThreadGroup();
+      Runnable startup = new StartupRunner(starterClassName, args);
+      Thread gedthread = new Thread(group, startup, "startup");
+      gedthread.setContextClassLoader(serviceLoader);
+
+      // ... und starten
+      gedthread.start();
+    }
   }
 
+  /**
+   * Hilfsklasse um unerwartet auftretende Exceptions zu verarbeiten.
+   */
   private static class GedPrintThreadGroup extends ThreadGroup
   {
     public GedPrintThreadGroup()
