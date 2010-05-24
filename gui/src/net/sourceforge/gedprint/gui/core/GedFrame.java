2f6addaaa762bd4a2352c29fc7e7a4e37471fb35
Jens Hofschröer <1849220+nigjo@users.noreply.github.com>
2010-05-24 17:29:21 +0000

    super.dispose() erfolgt erst, wenn alle anderen Fenster geschlossen sind. einige Kommentare gelöscht

--- gui/src/net/sourceforge/gedprint/gui/core/GedFrame.java
+++ gui/src/net/sourceforge/gedprint/gui/core/GedFrame.java
@@ -1,27 +1,23 @@
 package net.sourceforge.gedprint.gui.core;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.beans.PropertyVetoException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
@@ -58,10 +54,11 @@ public class GedFrame extends JFrame
     // Icons setzen
     ArrayList<Image> icons = new ArrayList<Image>();
     String[] iconResNames = new String[]
-    { "icon16.png", //$NON-NLS-1$
-        "icon32.png", //$NON-NLS-1$
-        "icon48.png", //$NON-NLS-1$
-        "icon256.png", //$NON-NLS-1$
+    {
+      "icon16.png", //$NON-NLS-1$
+      "icon32.png", //$NON-NLS-1$
+      "icon48.png", //$NON-NLS-1$
+      "icon256.png", //$NON-NLS-1$
     };
     MediaTracker mt = new MediaTracker(this);
     for(String iconResName : iconResNames)
@@ -106,113 +103,20 @@ public class GedFrame extends JFrame
     getContentPane().add(status, BorderLayout.SOUTH);
   }
 
-  /*
-   * private void addGedPainter(String painterClassName) { try { Class cl =
-   * Class.forName(painterClassName); drawPanel = (GedPainter) cl.newInstance();
-   * } catch(Exception e) { throw new IllegalStateException(e); }
-   * if(drawPanel.isScrollable()) getContentPane().add(new
-   * JScrollPane(drawPanel)); else getContentPane().add(drawPanel);
-   * 
-   * ActionManager.setActionProperty("painter", drawPanel); }
-   */
-
-  // /**
-  // * @param gedfile
-  // * @deprecated
-  // */
-  // public void setGedFile(GedFile gedfile)
-  // {
-  // //setStartID(null);
-  // //this.ged = gedfile;
-  // ActionManager.setActionProperty(BasicAction.PROPERTY_FILE, gedfile);
-  // }
-
-  // /**
-  // * @param string
-  // * @deprecated
-  // */
-  // public void setStartID(String string)
-  // {
-  // drawPanel.clearAll();
-  //
-  // GedFile ged = (GedFile)
-  // ActionManager.getActionProperty(BasicAction.PROPERTY_FILE);
-  // // Abbrechen, wenn keine GEDCOM Datei oder
-  // // keine ID angegeben ist.
-  // if(ged == null || string == null)
-  // {
-  //      ActionManager.performAction("AddRecordAction", null); //$NON-NLS-1$
-  // //ActionManager.setActionProperty(BasicAction.PROPERTY_RECORD, null);
-  // //updatePanel();
-  // return;
-  // }
-  //
-  // Record r = ged.findID(string);
-  // if(r instanceof Individual)
-  // {
-  // Individual indi = (Individual) r;
-  // Logger.getLogger(getClass().getName()).info(indi.getClearedFullName());
-  //      Logger.getLogger(getClass().getName()).info("Age: " + indi.getAge()); //$NON-NLS-1$
-  //
-  // drawPanel.addRecord(indi);
-  // }
-  // else if(r instanceof Family)
-  // {
-  // Family fam = (Family) r;
-  // Logger.getLogger(getClass().getName()).fine(fam.toString());
-  //
-  // // Die Familie selbst
-  // drawPanel.addRecord(fam);
-  //
-  // if(fam.getChildrenCount() > 0)
-  // {
-  // // und die Familien der Kinder.
-  // for(Family family : fam.getChildFamilies())
-  // {
-  // drawPanel.addRecord(family);
-  // }
-  // }
-  // }
-  // else if(r == null)
-  // {
-  //      Logger.getLogger(getClass().getName()).fine("no record found"); //$NON-NLS-1$
-  // }
-  // else
-  // {
-  // Logger.getLogger(getClass().getName()).fine(r.toString());
-  // }
-  //
-  // //ActionManager.setActionProperty(BasicAction.PROPERTY_RECORD, r);
-  //    ActionManager.performAction("AddRecordAction", r); //$NON-NLS-1$
-  // updatePanel();
-  // }
-
-  // /**
-  // * @deprecated
-  // */
-  // private void updatePanel()
-  // {
-  // if(isVisible())
-  // {
-  // repaint();
-  // validate();
-  // }
-  // }
-
   @Override
   public void dispose()
   {
     if(isReadyToExit())
     {
-      super.dispose();
       Frame[] frames = getFrames();
       for(Frame frame : frames)
       {
-        if(frame.isVisible())
+        if(frame != this && frame.isVisible())
         {
           return;
         }
       }
+      super.dispose();
 
       // wenn ich hier angekommen bin, ist kein Fenster mehr sichtbar.
       Logger logger = Logger.getLogger(GedPrintGui.class.getName());
@@ -252,7 +156,7 @@ public class GedFrame extends JFrame
 
     private void doRecord(PropertyChangeEvent evt)
     {
-      Record rec = (Record) evt.getNewValue();
+      Record rec = (Record)evt.getNewValue();
       if(rec == null)
         text.setText(DEFAULT);
       else if(rec instanceof Individual)
@@ -267,57 +171,16 @@ public class GedFrame extends JFrame
     dispose();
   }
 
-  // /**
-  // * @deprecated
-  // */
-  // public Record getRecord()
-  // {
-  // if(drawPanel == null)
-  // return null;
-  // return drawPanel.getRecord();
-  // }
   public class GedDesktop extends JDesktopPane
   {
     private static final long serialVersionUID = -1454766223860345991L;
-
     DocumentManager manager;
 
     public GedDesktop()
     {
       super();
       manager = new DocumentManager(this);
+      setBackground(Color.LIGHT_GRAY);
     }
-
-//    @Override
-//    protected void paintComponent(Graphics g)
-//    {
-//      super.paintComponent(g);
-//
-//      Dimension size = getSize();
-//
-//      Image biggest = null;
-//      List<Image> icons = GedFrame.this.getIconImages();
-//      for(Image icon : icons)
-//      {
-//        if(biggest == null)
-//        {
-//          biggest = icon;
-//        }
-//        else
-//        {
-//          int iwidth = icon.getWidth(this);
-//          int bwidth = biggest.getWidth(this);
-//          if(iwidth > bwidth)
-//            biggest = icon;
-//        }
-//      }
-//
-//      int x = (size.width - biggest.getWidth(this) * 2) / 2;
-//      int y = (size.height - biggest.getHeight(this) * 2) / 2;
-//
-//      g.drawImage(biggest, x, y, biggest.getWidth(this) * 2, biggest
-//          .getHeight(this) * 2, this);
-//    }
-    
   }
 }
