f84c8f7b8e2e84a0de738bf8cb63ad813f7b781c
Jens Hofschröer <1849220+nigjo@users.noreply.github.com>
2010-05-24 17:27:01 +0000

    Dialog mit Buttons ausgestattet aber nicht mehr eigenständig, sondern Teil der neuen Action Basisklasse "DialogAction"

--- gui/src/net/sourceforge/gedprint/gui/action/SearchIndividual.java
+++ gui/src/net/sourceforge/gedprint/gui/action/SearchIndividual.java
@@ -1,19 +1,25 @@
 package net.sourceforge.gedprint.gui.action;
 
-import java.awt.event.ActionEvent;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 
-import javax.swing.Action;
-
-public class SearchIndividual extends BasicAction
+public class SearchIndividual extends DialogAction
 {
   private static final long serialVersionUID = 4316704368424658584L;
 
-  public void actionPerformed(ActionEvent e)
+  @Override
+  public JComponent getContentPane()
   {
-    ActionDialog dlg = new ActionDialog(this);
-    
-    dlg.setVisible(true);
-    Action selected = dlg.getSelectedAction();
+    return new JLabel("Hallo Welt");
   }
 
+  @Override
+  protected void load()
+  {
+  }
+
+  @Override
+  protected void store()
+  {
+  }
 }
